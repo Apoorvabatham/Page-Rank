af5ad6cadbb41aceca963e671c32a83e20328bc2
apoorva batham <apba00001@uni-saarland.de>
2024-06-09 23:37:38 +0200

    sqdw

--- src/randomsurfer.c
+++ src/randomsurfer.c
@@ -6,7 +6,6 @@
 #include "graph.h"
 
     struct Visited {
-    char name[256];
     float times;
     };
 
@@ -18,8 +17,6 @@ void simulate_rs (graph_t *graph, int no_steps, double p) {
     struct Visited visited[graph->count];
 
     for(int i=0; i<graph->count; i++){
-        strncpy(visited[i].name ,graph->vertices[i]->name, sizeof(visited[i].name) - 1);
-        visited[i].name[sizeof(visited[i].name)-1]= '\0';
         visited[i].times=0;
     }
 
@@ -29,25 +26,24 @@ void simulate_rs (graph_t *graph, int no_steps, double p) {
         if( randu(RAND_MAX/RAND_MAX)< p){
             current_s= randu(graph->count);
         }else{
-            if (graph->vertices[current_s]->num_edges!= 0) {
+            if (graph->vertices[current_s]->num_edges==0) {
+                current_s= randu(graph->count);
+            }else {
             int edge_index = randu( graph-> vertices[current_s]-> num_edges);
            vertice_t *next_vertice = graph->vertices[current_s]->out_edges[edge_index];
 
            for(int j=0; j<graph->count; j++){
                 if (graph->vertices[j]== next_vertice){
                     current_s= j;
-                    break;
                 }
            }
-            }else {
-                current_s= randu(graph->count);
             }
         }
         visited[current_s].times++;
     }
     for(int i=0; i<graph->count; i++){
-    printf("%s ",visited[i].name);
-    printf("%.10f\n",(double)(visited[i].times)/(double)no_steps);
+    printf("%s ",graph->vertices[i]->name);
+    printf("%.10f\n",(double)(visited[i].times)/no_steps);
     }
 
 }
